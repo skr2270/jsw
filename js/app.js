2b07737edb11eeff0a579bf2318374fab644335e
Sai Kumar Rayavarapu <sk.r.2270@gmail.com>
2021-04-02 14:28:54 +0530

    Swapped files

--- js/app.js
+++
@@ -1,9 +0,0 @@
-const controller = new ScrollMagic.Controller();
-
-const exploreScene = new ScrollMagic.Scene({
-    triggerElement: ".hike-exp",
-    triggerHook: 0.5
-})
-    .addIndicators({ colorStart: 'white', colorTrigger: "white" })
-    .setClassToggle('.hike-exp', "active")
-    .addTo(controller);
--- js/exp.js
+++ js/exp.js
@@ -40,3 +40,13 @@
 // }
 
 // observer.observe(slide);
+//4
+const controller = new ScrollMagic.Controller();
+
+const exploreScene = new ScrollMagic.Scene({
+    triggerElement: ".hike-exp",
+    triggerHook: 0.5
+})
+    .addIndicators({ colorStart: 'white', colorTrigger: "white" })
+    .setClassToggle('.hike-exp', "active")
+    .addTo(controller);
